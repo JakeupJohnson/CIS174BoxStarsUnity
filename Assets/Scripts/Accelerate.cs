ac7ecbe2401360b5341f3e49c4176451aba6d6c0
jacobejohnson <jacob.johnson365@gmail.com>
2019-04-24 23:33:21 -0500

    initial commit

---
+++ Assets/Scripts/Accelerate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerate : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.back * 1000.0f);
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    public float startingPosition = 0f;
+    public float swingPosition = 45f;
+    public float hitStrength = 10000f;
+    public float paddleDamper = 150f;
+    HingeJoint hinge;
+    public string inputName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = paddleDamper;
+
+        if (Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = swingPosition;
+        }
+        else
+        {
+            spring.targetPosition = startingPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+
+    }
+}
